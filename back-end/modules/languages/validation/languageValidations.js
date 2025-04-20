0ee2baa743ccf776eb80b1e8515b70799740d62b
IK32K3 <hoangviet232003@gmail.com>
2025-04-20 19:23:33 +0700

    Update

--- back-end/modules/languages/validation/languageValidations.js
+++ back-end/modules/languages/validation/languageValidations.js
@@ -2,8 +2,8 @@ const { BodyWithLocale, ParamWithLocale, QueryWithLocale } = require('kernels/ru
 
 // Get all languages validation
 const getAllLanguagesValidation = [
-    new QueryWithLocale('page').isNumberic().get(),
-    new QueryWithLocale('limit').isNumberic().get()
+    new QueryWithLocale('page').optional().isNumeric().get(),
+    new QueryWithLocale('limit').optional().isNumeric().get()
 ];
 
 // Get language by ID validation
